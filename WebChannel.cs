855e45c5836fc20602df84d3763efa2c30f59259
john <john@mooseandlemur.com>
2021-09-02 18:04:26 +0100

    Add project files.

---
+++ WebChannel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using BetterAsync;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BetterNetworking
+{
+    public class WebChannel
+    {
+        private BinaryFormatter bf = new BinaryFormatter();
+        public Socket socket;
+        private NetworkStream stream;
+        public int trafficCount
+        {
+            get => items.Count;
+        }
+        public WebChannel(IPEndPoint destination)
+        {
+            socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            stream = new NetworkStream(socket);
+            Flow.Go(() => {
+                socket.Connect(destination);
+            });
+        }
+        public void Send<T>(T data)
+        {
+            bf.Serialize(stream,data);
+        }
+        public Promise<T> Receive<T>()
+        {
+            return new Promise<T>((resolve, reject) =>
+            {
+
+            });
+        }
+    }
+    public class WebReceiver
+    {
+
+    }
+}
